7970f22eceff64e61c26d14d9aafe81e79fa22da
Bruno Lira <bruno.padovan@gmail.com>
2019-04-25 20:43:54 -0300

    First Commit

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,48 @@
+const { User, Appointment } = require('../models')
+const { Op } = require('sequelize')
+const moment = require('moment')
+
+class AppointmentController {
+  async create (req, res) {
+    const provider = await User.findByPk(req.params.provider)
+
+    return res.render('appointments/create', { provider })
+  }
+
+  async store (req, res) {
+    const { id } = req.session.user
+    const { provider } = req.params
+    const { date } = req.body
+
+    await Appointment.create({
+      user_id: id,
+      provider_id: provider,
+      date
+    })
+
+    return res.redirect('/app/dashboard')
+  }
+
+  async show (req, res) {
+    const appointments = await Appointment.findAll({
+      include: [{ model: User }],
+      where: {
+        provider_id: req.session.user.id,
+        date: {
+          [Op.between]: [
+            moment()
+              .startOf('day')
+              .format(),
+            moment()
+              .endOf('day')
+              .format()
+          ]
+        }
+      }
+    })
+
+    return res.render('appointments/show', { appointments })
+  }
+}
+
+module.exports = new AppointmentController()
